8e37eaf9f87eac18c02017bc7167db0db1486c7e
Kirill <V-Troitsky@yandex.ru>
2020-09-16 10:47:09 +0300

    Create schemes and models of user and article

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+// Подключение Express
 const express = require('express');
+// Подключение ODM Mongoose
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+// Подключение к серверу mongo
+mongoose.connect('mongodb://localhost:27017/newsexplorerdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => console.log(`Порт запущенного сервера: ${PORT}`));
