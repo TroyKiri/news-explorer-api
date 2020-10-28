c3c3d77cf5a76c2c8384e92b76cca2800ebf732a
Kirill <V-Troitsky@yandex.ru>
2020-10-28 08:53:36 +0300

    fix CORS v4

--- app.js
+++ app.js
@@ -55,10 +55,17 @@ mongoose.connect(config.adressMongo, {
 //   next();
 // });
 
-app.use(cors({
-  origin: ALLOWED_CORS,
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: ALLOWED_CORS,
+//   credentials: true,
+// }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  next();
+});
 
 app.use(requestLogger);
 
